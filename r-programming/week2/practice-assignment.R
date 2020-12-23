aa0654b9f4530b783236d40b3461ed9136748a5c
Ryan Yancey <ryanjyancey@gmail.com>
2020-12-23 11:10:55 -0800

    First commit

---
+++ r-programming/week2/practice-assignment.R
@@ -0,0 +1,7 @@
+setwd("r-programming/week2")
+
+dataset_url <- "http://s3.amazonaws.com/practice_assignment/diet_data.zip"
+download.file(dataset_url, "diet_data.zip")
+unzip("diet_data.zip", exdir = "diet_data")
+
+list.files("diet_data")
