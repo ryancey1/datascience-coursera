8058c0510dc2ffcd73c07af487310bfafe9b23ad
Ryan Yancey <ryanjyancey@gmail.com>
2020-12-26 11:24:47 -0800

    Updated code in pollutantmean.R

--- r-programming/week2/programming-assignment/pollutantmean.R
+++ r-programming/week2/programming-assignment/pollutantmean.R
@@ -1,34 +1,14 @@
-## 'directory' is a character vector of length 1 indicating
-## the location of the CSV files
-
-## 'pollutant' is a character vector of length 1 indicating
-## the name of the pollutant for which we will calculate the
-## mean; either "sulfate" or "nitrate".
-
-## 'id' is an integer vector indicating the monitor ID numbers
-## to be used
-
-## Return the mean of the pollutant across all monitors list
-## in the 'id' vector (ignoring NA values)
-## NOTE: Do not round the result!
-
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-     ## vector of file paths
-     files <- list.files(directory, full.names = TRUE)
-     ## empty vector to fill with means
-     mean.vect <- vector("numeric", length = length(id))
-
-     ## calculates mean of specified columns
-     for (i in seq_along(id)) {
-          tmp <- read.csv(files[id[i]])
-          item <- mean(tmp[[pollutant]], na.rm = TRUE)
-          mean.vect[i] <- item
-          ## if-else to deal with files without pollutant values (NaNs)
-          # if(is.nan(item)) {
-          #      mean.vect[i] <- 0
-          # } else {
-          #      mean.vect[i] <- item
-          # }
-     }
-     mean(mean.vect, na.rm = TRUE)
+        
+        ## create sub-setted list of files based on id
+        files <- list.files(directory, full.names = TRUE)[id]
+        
+        ## extract pollutant columns from specified file into list
+        data <- lapply(files, function(x) read.csv(x)[[pollutant]])
+        
+        ## un-list
+        data <- unlist(data)
+        
+        ## calculate weighted mean
+        mean(data, na.rm = TRUE)
 }
