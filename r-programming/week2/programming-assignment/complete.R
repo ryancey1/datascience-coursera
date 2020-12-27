893adfd2b760466b8923afe93205e101f8e6dca6
Ryan Yancey <ryanjyancey@gmail.com>
2020-12-26 19:00:08 -0800

    Simplified code. Tested output.

--- r-programming/week2/programming-assignment/complete.R
+++ r-programming/week2/programming-assignment/complete.R
@@ -3,17 +3,15 @@ complete <- function(directory, id = 1:332) {
      files <- list.files(directory, full.names = TRUE)[id]
      
      ## storage vectors for ids and nobs
-     ids <- vector(mode="numeric", length = length(id))
      nobs <- vector(mode = "numeric", length = length(id))
      
      ## reads CSV file, calculates complete cases, adds to storage
      for(i in seq_along(id)) {
           tmp <- read.csv(files[i])
-          comp <- sum(complete.cases(tmp[,2:3]))
-          ids[i] <- id[i]
-          nobs[i] <- comp
+          nob <- sum(complete.cases(tmp[,2:3]))
+          nobs[i] <- nob
      }
      
      ## generates data frame
-     data.frame(id = ids, nobs = nobs)
+     data.frame(id = id, nobs = nobs)
 }
