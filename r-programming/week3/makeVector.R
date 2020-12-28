9432f2f5f3c49aac6b3742f845276e27fd76183c
Ryan Yancey <ryanjyancey@gmail.com>
2020-12-27 19:57:51 -0800

    Finished code.

--- r-programming/week3/makeVector.R
+++ r-programming/week3/makeVector.R
@@ -11,3 +11,15 @@ makeVector <- function(x = numeric()) {
              setmean = setmean,
              getmean = getmean)
 }
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
