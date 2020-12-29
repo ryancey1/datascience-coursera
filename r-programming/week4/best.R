738be19fcf8e35c371810d4eae6be57bb4364578
Ryan Yancey <ryanjyancey@gmail.com>
2020-12-28 16:58:41 -0800

    Minor code tweaks. Added header comment.

--- r-programming/week4/best.R
+++ r-programming/week4/best.R
@@ -1,10 +1,15 @@
+## This function take two arguments: the 2-character abbreviated name of a 
+## state and an outcome name. The function reads the outcome-of-care-measures.csv 
+## file and returns a character vector with the name of the hospital that has 
+## the best (i.e. lowest) 30-day mortality for the specified outcome in that state.
+
 best <- function(state, outcome) {
-        ## Read and format outcome data, keep desired columns
+        ## Reads and formats outcome data -- keeps desired columns
         data <- list.files("r-programming/week4/rprog_data_ProgAssignment3-data",
                            full.names = TRUE, pattern = ".csv")
         data <- read.csv(data[2])[c(2,7,11,17,23)]
         
-        ## Coerce data columns to numeric, suppress warnings
+        ## Coerces data columns to numeric, suppress warnings
         suppressWarnings(for(i in 3:5) {
                 data[,i] <- as.numeric(data[,i])
         })        
@@ -13,7 +18,7 @@ best <- function(state, outcome) {
         valid.outcomes <- c("heart attack", "heart failure", "pneumonia")
         valid.states <- state.abb
         
-        ## Check that state and outcome are valid
+        ## Checks that state and outcome are valid
         if(!any(state == valid.states)) {
                 stop("invalid state")
         }
@@ -21,22 +26,25 @@ best <- function(state, outcome) {
                 stop("invalid outcome")
         }
         
-        ## Split and subset 
+        ## Splits and subsets
         byState <- split(data, data$State)
         byState <- byState[[state]]
         
-        ## Subsets 
+        ## Sorts subsetted data in ascending order 
         if(outcome == valid.outcomes[1]) {
                 sorted <- byState[,c(1,3)]
                 sorted <- sorted[order(sorted[,2], sorted[,1]), ]
-                sorted[1,1]
+                sorted <- sorted[1,1]
         } else if(outcome == valid.outcomes[2]) {
                 sorted <- byState[,c(1,4)]
                 sorted <- sorted[order(sorted[,2], sorted[,1]), ]
-                sorted[1,1]
+                sorted <- sorted[1,1]
         } else if(outcome == valid.outcomes[3]) {
                 sorted <- byState[,c(1,5)]
                 sorted <- sorted[order(sorted[,2], sorted[,1]), ]
-                sorted[1,1]
+                sorted <- sorted[1,1]
         }
+        
+        # Returns top result
+        sorted
 }
