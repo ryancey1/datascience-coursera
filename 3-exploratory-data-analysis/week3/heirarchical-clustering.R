046f01db8b6dd8658801f4bc37373f9a04d0089f
Ryan Yancey <ryanjyancey@gmail.com>
2021-01-08 16:04:54 -0800

    Aliases to lecture material, scripts for clustering

---
+++ 3-exploratory-data-analysis/week3/heirarchical-clustering.R
@@ -0,0 +1,33 @@
+# Heirarchical Clustering -------------------------------------------------
+setwd("3-exploratory-data-analysis/week3/")
+set.seed(1234)
+
+# par(mar = c(0, 0, 0, 0))
+
+x <- rnorm(12, mean = rep(1:3, each = 4), sd = 0.2)
+y <- rnorm(12, mean = rep(c(1, 2, 1), each = 4), sd = 0.2)
+
+plot(x, y, col = "blue", pch = 19, cex = 2)
+text(x + 0.05, y + 0.05, labels = as.character(1:12))
+
+dataFrame <- data.frame(x = x, y = y)
+dist <- dist(dataFrame)
+hClustering <- hclust(dist)
+
+png(filename = "hclust.png")
+plot(hClustering)
+dev.off()
+
+source("myplclust.R")
+
+png(filename = "myplclust.png")
+myplclust(hClustering, lab = rep(1:3, each = 4), lab.col = rep(1:3, each = 4))
+dev.off()
+
+set.seed(143)
+dataMatrix <- as.matrix(dataFrame)[sample(1:12),]
+
+png(filename = "heatmap.png")
+heatmap(dataMatrix)
+dev.off()
+
