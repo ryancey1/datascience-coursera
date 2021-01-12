eae98418d82fe35c196b1c86171cdd63f42dbbd6
Ryan Yancey <ryanjyancey@gmail.com>
2021-01-12 08:31:49 -0800

    Updates to code

--- 3-exploratory-data-analysis/week3/kmeans-clustering.R
+++ 3-exploratory-data-analysis/week3/kmeans-clustering.R
@@ -1,9 +1,7 @@
 # K-Means Clustering ------------------------------------------------------
 
-# a simple example
 set.seed(1234)
 par(mar = c(0,0,0,0))
-# created 3 "clusters"
 x <- rnorm(12, mean = rep(1:3, each =4), sd = 0.2)
 y <- rnorm(12, mean = rep(c(1,2,1), each =4), sd = 0.2)
 
@@ -11,13 +9,30 @@ plot(x, y, col = "blue", pch = 19, cex =2)
 text(x + 0.05, y + 0.05, labels = as.character(1:12))
 
 dataFrame <- data.frame(x, y)
+
+## round 1
 kmeansObj <- kmeans(dataFrame, centers = 3)
 names(kmeansObj)
-
 kmeansObj$cluster
+## [1] 3 1 1 3 2 2 2 2 2 2 2 2
+
+## round 2
+kmeansObj <- kmeans(dataFrame, centers = 3)
+names(kmeansObj)
+kmeansObj$cluster
+## [1] 3 3 3 3 1 1 1 1 2 2 2 2
 
 par(mar = rep(0.2, 4))
 plot(x,y,col=kmeansObj$cluster, pch = 19, cex = 2)
 points(kmeansObj$centers, col = 1:3, pch = 3, cex = 3, lwd = 3)
-dev.copy(png, file = "kmeans.png")
-dev.off()
+
+set.seed(1234)
+dataMatrix <- as.matrix(dataFrame)[sample(1:12),]
+kmeansObj2 <- kmeans(dataMatrix, centers = 3)
+par(mfrow = c(1,2), mar = c(2, 4, 0.1, 0.1))
+
+image(t(dataMatrix)[, nrow(dataMatrix):1], yaxt = "n")
+image(t(dataMatrix)[, order(kmeansObj2$cluster)], yaxt = "n")
+
+
+
